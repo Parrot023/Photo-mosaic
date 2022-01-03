b556ffd5369aeee80688ac6e1a43d0edc73b2711
Parrot023 <Andreas081203@gmail.com>
2022-01-03 21:31:27 +0100

    Code to download a test sample of images

--- image_downloader.py
+++ image_downloader.py
@@ -2,4 +2,4 @@
 from bing_image_downloader import downloader
 
 
-downloader.download("Nature", limit=100, output_dir='images', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
+downloader.download("Nature landscape", limit=500, output_dir='images', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
